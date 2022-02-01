805cc6f6dfbb7e3e018b1dcf8c90389a785c93ee
gwiyeomgo <gwiyeomgo@gmail.com>
2022-02-01 18:33:48 +0900

    2022-02-01 lesson3-1 account

---
+++ accounts/account.go
@@ -0,0 +1,64 @@
+package accounts
+
+import "errors"
+
+type Account struct {
+	owner   string
+	balance int
+}
+
+// error 가 많다면 따로 변수로 만들어서 선언
+// 이때 error 이름은 err00 처럼 err 로 시작하도록 작명하여 코드퀄리티를 높힌다
+var errNoMoney = errors.New("Cant't withdraw you are poor")
+
+// Function
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+	//NewAccount 함수는 public 함수이다
+	//owner 라는 string 데이터 타입에 매개변수를 갖는다
+	//Account 의 주소값을 반환 한다
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+
+//go receiver
+// func 키워드 뒤에 () 안에 receiver를 써준다
+//Method
+//Method와 function은 동등한 관계이다
+
+//Deposit x amount on your account
+/*func (a Account) Deposit(amount int)  {
+	//Deposit function안에 a 라는 receiver를 가지고 있다
+	// a의 타입은 Account 이다.
+	// receiver는 struct 의 첫 글자를 따서 소문자로 지어야 한다 *
+	//main에서 Deposit을 호출하면
+	// a 는 Account 이지만
+	// main 에 선언한 account의 복사본이다
+	//실제 account 가 아니다
+
+	a.balance += amount
+}*/
+func (a *Account) Deposit(amount int) {
+	//main 에서 account.Deposit()을 했을 때
+	// 실제 account 를 가져오려면
+	//리시버의 타입에 *(포인터) 를 붙여준다
+	//account를 복사하는 것이 아닌
+	//Deposit method를 호출한 account를 사용한다
+	//go는 보안을 위해 복사본을 만든다
+
+	a.balance += amount
+}
+func (a Account) Balance() int {
+	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+	//-라면 출금하면 안되도록
+	//error handling 해야 한다
+	// nil 은 뭐지? error 는 error 와 nil 있으며 이것은 javascript none 과 같다
+}
--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/gwiyeomgo/go-scrapper/something"
+	"github.com/gwiyeomgo/go-scrapper/accounts"
+	"log"
 	"strings"
 )
 
@@ -20,11 +21,27 @@ func repeatMe(words ...string) {
 	fmt.Println(words)
 }
 func main() {
-	fmt.Println("Hello world")
+	/*fmt.Println("Hello world")
 	something.SayHello() //다른 패키지에서 export 된 func
 
 	totalLength, upperName := lenAndUpper("test")
 	fmt.Println(totalLength, upperName)
 
-	repeatMe("A", "B", "C", "D", "E")
+	repeatMe("A", "B", "C", "D", "E")*/
+
+	// go 에서 constructor 만드는 방법
+	account := accounts.NewAccount("gwiyeomgo")
+	//fmt.Println(account)
+	//&{gwiyeomgo 0}
+	//복사본을 return 하는 것이 아닌
+	//실제 메모리 주소(address)가 출력된다. 복사본이 아니라 object 를 return 시켰다
+	account.Deposit(10) //입금하다
+	//fmt.Println(account.Balance()) //10
+	//account.Withdraw(20) //인출하다
+	//fmt.Println(account.Balance()) //-10
+	//error 를 다루기 위해서
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err) // println을 호출하고 프로그램 종료 시킴
+	}
 }
